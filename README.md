66d97886c1edcfbea51828d8b76ef18267dbe82d
Mengxin1437 <3386716762@qq.com>
2024-06-01 15:53:27 +0800

    一些小改动

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ChessGame
 
 这是服务端
+
+Message对象不可以随意改变
--- src/server/Server.java
+++ src/server/Server.java
@@ -12,6 +12,7 @@ public class Server {
     private ArrayList<ArrayList<Socket>> clientss;
 
     public Server(){
+        System.out.println("服务端启动");
         clientss = new ArrayList<>();
         for(int i=0; i<3; i++){
             clientss.add(new ArrayList<>());
@@ -27,24 +28,28 @@ public class Server {
                 Object object = inputStream.readObject();
                 Message msg = (Message) object;
                 int type = msg.chessType;
-                System.out.println(msg);
+//                System.out.println(msg);
 
+                //加入到对应的集合里边
                 clientss.get(type).add(socket);
-                for(ArrayList<Socket> clients: clientss){
-                    if(clients.size() == 2){
-                        Match match = new Match(clients.get(0), clients.get(1));
-                        match.start();
-                        clients.clear();
-                    }
-                    //超时清掉socket
-                    new Thread(()->{
+                System.out.println("玩家加入了对局,类型为："+type);
+
+                //看对应集合里边是否符合对局要求
+                ArrayList<Socket> clients = clientss.get(type);
+                if (clients.size() == 2) {
+                    Match match = new Match(clients.get(0), clients.get(1));
+                    match.start();
+                    clients.clear();
+                }else {
+                    //从加入到集合里边开始，超时会自动被清掉
+                    new Thread(() -> {
                         try {
                             Thread.sleep(10000);
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
 
-                        if(clients.size()==1){
+                        if (clients.size() == 1) {
                             //向客户端发送匹配失败的信息
                             try {
                                 clients.get(0).getOutputStream().write(245);
